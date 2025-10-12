1c02c5f8f7104b3b6eaa228ec000424f4f237340
Vyacheslav <oseanic@yandex.ru>
2025-10-12 17:32:26 +0300

    add chi router

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,16 +2,21 @@ package app
 
 import (
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 	"github.com/vyacheslavskl/go-shorterner/internal/handler"
 )
 
 type App struct {
-	routes *http.ServeMux
+	routes *chi.Mux
 }
 
 func New() *App {
-	app := &App{routes: http.NewServeMux()}
-	app.routes.HandleFunc("/", handler.PostRoot)
+	app := &App{routes: chi.NewMux()}
+
+	app.routes.Get("/", handler.PostRoot)
+	app.routes.Get("/{id}", handler.PostRoot)
+	app.routes.Post("/", handler.PostRoot)
 	return app
 }
 
