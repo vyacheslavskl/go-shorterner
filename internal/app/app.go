1c2bba91bcaea29f73ff8a667ec6cec4983be92e
Vyacheslav <oseanic@yandex.ru>
2025-10-23 22:24:55 +0300

    add simple getenv

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"flag"
 	"net/http"
+	"os"
 
 	"github.com/vyacheslavskl/go-shorterner/internal/config"
 	"github.com/vyacheslavskl/go-shorterner/internal/handler"
@@ -13,7 +14,18 @@ import (
 func Run() error {
 	addr := &config.Config{
 		Address:         config.NetAddress{Protocol: "", Host: "localhost", Port: 8080},
-		RedirectAddress: config.NetAddress{Protocol: "http://", Host: "localhost", Port: 8080}}
+		RedirectAddress: config.NetAddress{Protocol: "http://", Host: "localhost", Port: 8080},
+	}
+
+	ser_adr := os.Getenv("SERVER_ADDRESS")
+	if ser_adr != "" {
+		_ = addr.Address.Set(ser_adr)
+	}
+
+	bas_adr := os.Getenv("BASE_URL")
+	if bas_adr != "" {
+		_ = addr.RedirectAddress.Set(ser_adr)
+	}
 
 	flag.Var(&addr.Address, "a", "Net address host:port")
 	flag.Var(&addr.RedirectAddress, "b", "Net address host:port")
