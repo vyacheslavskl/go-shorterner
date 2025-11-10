38a0b0c9f01f644e14c8b75b7a77e7aa8de985d6
vyacheslavskl <oseanic@yandex.ru>
2025-11-10 20:55:13 +0300

    Iter9 (#9)
    * add zap sugar middleware
    * add gzip middleware
    * refactor repo

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/vyacheslavskl/go-shorterner
 
 go 1.24.7
 
-require github.com/go-chi/chi/v5 v5.2.3
+require (
+	github.com/go-chi/chi/v5 v5.2.3
+	go.uber.org/zap v1.27.0
+)
+
+require (
+	github.com/google/uuid v1.6.0
+	go.uber.org/multierr v1.10.0 // indirect
+)
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,33 +1,111 @@
 package app
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/vyacheslavskl/go-shorterner/internal/config"
 	"github.com/vyacheslavskl/go-shorterner/internal/handler"
 	"github.com/vyacheslavskl/go-shorterner/internal/repository"
 	"github.com/vyacheslavskl/go-shorterner/internal/service"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func Run() error {
+	log := zap.NewDevelopmentConfig()
+	log.Level = zap.NewAtomicLevelAt(zapcore.InfoLevel)
+	logger, err := log.Build()
+	if err != nil {
+		return err
+	}
+	defer logger.Sync()
+
+	sugar := logger.Sugar()
+
 	addr := &config.Config{
 		Address:         config.NetAddress{Protocol: "", Host: "localhost", Port: 8080},
-		RedirectAddress: config.NetAddress{Protocol: "http://", Host: "localhost", Port: 8080}}
+		RedirectAddress: config.NetAddress{Protocol: "http://", Host: "localhost", Port: 8080},
+	}
+
+	serAdr := os.Getenv("SERVER_ADDRESS")
+	if serAdr != "" {
+		err = addr.Address.Set(serAdr)
+		if err != nil {
+			return err
+		}
+	}
+	basAdr := os.Getenv("BASE_URL")
+	if basAdr != "" {
+		err = addr.RedirectAddress.Set(basAdr)
+		if err != nil {
+			return err
+		}
+	}
+
+	storagePath := os.Getenv("FILE_STORAGE_PATH")
+	if storagePath == "" {
+		flag.StringVar(&storagePath, "f", "urls.json", "path to storage urls")
+	}
 
 	flag.Var(&addr.Address, "a", "Net address host:port")
 	flag.Var(&addr.RedirectAddress, "b", "Net address host:port")
 	flag.Parse()
 
 	cfg := config.GetConfig(addr)
-	repo := repository.NewMapRepo()
-	srv := service.NewService(repo)
+	sugar.Infow("Started with config",
+		"Address", cfg.Address.String(),
+		"RedirectAddress", cfg.RedirectAddress.String(),
+		"StoragePath", storagePath,
+	)
 
-	handler := handler.NewShorterHandler(cfg, srv)
-
-	err := http.ListenAndServe(cfg.Address.String(), handler.Routes())
+	repo, err := repository.NewMapRepo(storagePath)
 	if err != nil {
 		return err
 	}
-	return nil
+	srv := service.NewService(repo)
+	handler := handler.NewShorterHandler(cfg, srv, sugar)
+
+	server := &http.Server{
+		Addr:    cfg.Address.String(),
+		Handler: handler.Routes(),
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	errCh := repo.PeriodicSave(10 * time.Second)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			sugar.Errorln(err.Error())
+		}
+	}()
+
+	for {
+		select {
+		case <-stop:
+			shutdownCtx := context.Background()
+			if err := server.Shutdown(shutdownCtx); err != nil {
+				return err
+			} else {
+				repo.SaveData()
+				sugar.Infoln("Graceful shutdown")
+				return nil
+			}
+		case err, ok := <-errCh:
+			if !ok {
+				sugar.Infoln("Err channel is closed")
+				return errors.New("errCh channel is closed")
+			}
+			sugar.Infoln("Error when saving data:", err)
+			return err
+		}
+	}
+
 }
