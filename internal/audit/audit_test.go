3970dabfee77a8ac25cb8d4d72c9631b134e0583
Vyacheslav Sokolov <oseanic@yandex.ru>
2026-04-06 22:54:35 +0300

    remove fileObs tests

--- internal/audit/audit_test.go
+++ internal/audit/audit_test.go
@@ -2,11 +2,7 @@ package audit
 
 import (
 	"context"
-	"encoding/json"
-	"errors"
 	"net/http"
-	"os"
-	"sync"
 	"testing"
 	"time"
 
@@ -15,7 +11,6 @@ import (
 	models "github.com/vyacheslavskl/go-shorterner/internal/model"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestPublisher_SubscribeAndNotify(t *testing.T) {
@@ -66,57 +61,6 @@ func TestFileObserver_NewFileObserver_FileCreateError(t *testing.T) {
 	observer, err := NewFileObserver("/invalid-path/forbidden-file.log")
 	assert.Nil(t, observer)
 	assert.Error(t, err)
-}
-
-func TestFileObserver_OnEvent_Success(t *testing.T) {
-	tmpfile, err := os.CreateTemp("", "audit_*.log")
-	require.NoError(t, err)
-	defer os.Remove(tmpfile.Name())
-	defer tmpfile.Close()
-
-	observer := &FileObserver{file: tmpfile, mu: sync.Mutex{}}
-
-	event := models.AuditEvent{
-		Action: AuditActionShorten,
-		UserID: "user-456",
-		URL:    "https://example.com",
-	}
-
-	err = observer.OnEvent(context.Background(), event)
-	require.NoError(t, err)
-
-	// Перечитываем файл
-	data, err := os.ReadFile(tmpfile.Name())
-	require.NoError(t, err)
-
-	var loggedEvent models.AuditEvent
-	err = json.Unmarshal(data[:len(data)-1], &loggedEvent) // убираем \n
-	require.NoError(t, err)
-
-	assert.Equal(t, event.Action, loggedEvent.Action)
-	assert.Equal(t, event.UserID, loggedEvent.UserID)
-	assert.Equal(t, event.URL, loggedEvent.URL)
-}
-
-func TestFileObserver_OnEvent_WriteError(t *testing.T) {
-	// Мокаем *os.File, чтобы Write возвращал ошибку
-	// badWriter := &BadWriter{}
-	file := os.NewFile(1, "badfile") // некорректный дескриптор
-	defer file.Close()
-
-	observer := &FileObserver{file: file, mu: sync.Mutex{}}
-
-	event := models.AuditEvent{Action: "shorten", UserID: "user"}
-
-	err := observer.OnEvent(context.Background(), event)
-	assert.Error(t, err)
-}
-
-// BadWriter — имитация сбоя при записи
-type BadWriter struct{}
-
-func (bw *BadWriter) Write(p []byte) (n int, err error) {
-	return 0, errors.New("write failed")
 }
 
 func TestHTTPObserver_OnEvent_RequestError(t *testing.T) {
